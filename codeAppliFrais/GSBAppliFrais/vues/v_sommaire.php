57dd78fdd69aedad41d45ef07285f41aa0c498cf
Martin Top <Martin.Top.MT@gmail.com>
2013-05-17 09:16:56 +0200

    Update 17/05/2013

--- codeAppliFrais/GSBAppliFrais/vues/v_sommaire.php
+++ codeAppliFrais/GSBAppliFrais/vues/v_sommaire.php
@@ -9,7 +9,7 @@
             <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
         </li>
          <li class="smenu">
-              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+              <a href="index.php?uc=gererFrais&action=ajoutFraisForfait" title="Saisie fiche de frais ">Saisie fiche de frais</a>
            </li>
            <li class="smenu">
               <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
